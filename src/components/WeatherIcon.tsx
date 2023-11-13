6972db47de1716ea7bf97526bce73cf5d2f390da
Jihoon <jangna96@gmail.com>
2023-11-13 09:41:51 +0900

    feat: 날씨 아이콘 추가
    - 영하 + 구름양 9이상 일 때 눈 아이콘 추가
    - cloud, sunCloud 순서 변경

--- src/components/WeatherIcon.tsx
+++ src/components/WeatherIcon.tsx
@@ -9,21 +9,25 @@ export const WeatherIcon: React.FC<WeatherIconProps> = ({ fcstValue }) => {
   if (Number(fcstValue[1]) >= 9) {
     return (
       <StyledWeatherIcon>
-        <img src={require("../assets/icon_rain.png")} alt="비" />
+        {Number(fcstValue[0]) < 0 ? (
+          <img src={require("../assets/icon_snow.png")} alt="눈" />
+        ) : (
+          <img src={require("../assets/icon_rain.png")} alt="비" />
+        )}
         <p>{fcstValue[2]}%</p>
       </StyledWeatherIcon>
     );
   } else if (Number(fcstValue[1]) >= 6) {
     return (
       <StyledWeatherIcon>
-        <img src={require("../assets/icon_suncloud.png")} alt="매우흐림" />
+        <img src={require("../assets/icon_cloud.png")} alt="매우흐림" />
         <p>{fcstValue[2]}%</p>
       </StyledWeatherIcon>
     );
   } else if (Number(fcstValue[1]) >= 3) {
     return (
       <StyledWeatherIcon>
-        <img src={require("../assets/icon_cloud.png")} alt="흐림" />
+        <img src={require("../assets/icon_suncloud.png")} alt="흐림" />
         <p>{fcstValue[2]}%</p>
       </StyledWeatherIcon>
     );
