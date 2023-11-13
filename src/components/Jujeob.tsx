2ea8dd816b45800c58eee8e1326b1a9674487d4c
Jihoon <jangna96@gmail.com>
2023-11-13 10:26:46 +0900

    fix: 주접 멘트 에러 픽스
    - 영하일 때 데이터가 0도 이상일 때 데이터로 나오는 에러 픽스

--- src/components/Jujeob.tsx
+++ src/components/Jujeob.tsx
@@ -27,40 +27,55 @@ interface JujeobProps {
 export const Jujeob: React.FC<JujeobProps> = ({ fcstValue }) => {
   const [jujeob, setJujeob] = useState<string>("");
   useEffect(() => {
+    // 20도 이상, 비,눈
     if (Number(fcstValue[0]) >= 20 && Number(fcstValue[1]) >= 9) {
       setJujeob(jujeobRain20[Math.floor(Math.random() * 4)]);
+      // 20도 이상, 흐림
     } else if (Number(fcstValue[0]) >= 20 && Number(fcstValue[1]) >= 6) {
       setJujeob(jujeobCloud20[Math.floor(Math.random() * 4)]);
+      // 20도 이상, 약간 흐림
     } else if (Number(fcstValue[0]) >= 20 && Number(fcstValue[1]) >= 3) {
       setJujeob(jujeobSunCloud20[Math.floor(Math.random() * 4)]);
+      // 20도 이상, 맑음
     } else if (Number(fcstValue[0]) >= 20 && Number(fcstValue[1]) >= 0) {
       setJujeob(jujeobSun20[Math.floor(Math.random() * 4)]);
+      // 10도 이상, 비,눈
     } else if (Number(fcstValue[0]) >= 10 && Number(fcstValue[1]) >= 9) {
       setJujeob(jujeobRain10[Math.floor(Math.random() * 4)]);
+      // 10도 이상, 흐림
     } else if (Number(fcstValue[0]) >= 10 && Number(fcstValue[1]) >= 6) {
       setJujeob(jujeobCloud10[Math.floor(Math.random() * 4)]);
+      // 10도 이상, 약간 흐림
     } else if (Number(fcstValue[0]) >= 10 && Number(fcstValue[1]) >= 3) {
       setJujeob(jujeobSunCloud10[Math.floor(Math.random() * 4)]);
+      // 10도 이상, 맑음
     } else if (Number(fcstValue[0]) >= 10 && Number(fcstValue[1]) >= 0) {
       setJujeob(jujeobSun10[Math.floor(Math.random() * 4)]);
+      // 0도 이상, 비,눈
     } else if (Number(fcstValue[0]) >= 0 && Number(fcstValue[1]) >= 9) {
       setJujeob(jujeobRain0[Math.floor(Math.random() * 4)]);
+      // 0도 이상, 흐림
     } else if (Number(fcstValue[0]) >= 0 && Number(fcstValue[1]) >= 6) {
       setJujeob(jujeobCloud0[Math.floor(Math.random() * 4)]);
+      // 0도 이상, 약간 흐림
     } else if (Number(fcstValue[0]) >= 0 && Number(fcstValue[1]) >= 3) {
       setJujeob(jujeobSunCloud0[Math.floor(Math.random() * 4)]);
+      // 0도 이상, 맑음
     } else if (Number(fcstValue[0]) >= 0 && Number(fcstValue[1]) >= 0) {
       setJujeob(jujeobSun0[Math.floor(Math.random() * 15)]);
-    } else if (Number(fcstValue[0]) < 0 && Number(fcstValue[1]) >= 0) {
-      setJujeob(jujeobSun0[Math.floor(Math.random() * 4)]);
+      // 영하, 비,눈
     } else if (Number(fcstValue[0]) < 0 && Number(fcstValue[1]) >= 9) {
       setJujeob(jujeobRainMinus[Math.floor(Math.random() * 4)]);
+      // 영하, 흐림
     } else if (Number(fcstValue[0]) < 0 && Number(fcstValue[1]) >= 6) {
       setJujeob(jujeobCloudMinus[Math.floor(Math.random() * 4)]);
+      // 영하, 약간 흐림
     } else if (Number(fcstValue[0]) < 0 && Number(fcstValue[1]) >= 3) {
       setJujeob(jujeobSunCloudMinus[Math.floor(Math.random() * 4)]);
+      // 영하, 맑음
     } else if (Number(fcstValue[0]) < 0 && Number(fcstValue[1]) >= 0) {
       setJujeob(jujeobSunMinus[Math.floor(Math.random() * 4)]);
+      // 기타
     } else {
       setJujeob(jujeobRandom[Math.floor(Math.random() * 4)]);
     }
